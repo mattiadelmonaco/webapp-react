8c052b80a134164722473f0a8eac919c738cb624
mattiadelmonaco <mattia.delmonaco@gmail.com>
2025-02-25 17:24:32 +0100

    feat: add social footer btns, import in footer, add style and elements in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,17 @@
+import { Link } from "react-router";
+import SocialBtnRightFooter from "./SocialBtnRightFooter";
+
 export default function Footer() {
-  return <footer>Footer</footer>;
+  return (
+    <footer className="bg-black rounded-tl-4xl rounded-tr-4xl">
+      <div className="container">
+        <div className="flex justify-between">
+          <Link to="/">
+            <img className="h-25" src="/logo-webapp.svg" alt="logo 138movies" />
+          </Link>
+          <SocialBtnRightFooter />
+        </div>
+      </div>
+    </footer>
+  );
 }
---
+++ src/components/SocialBtnRightFooter.jsx
@@ -0,0 +1,32 @@
+export default function SocialBtnRightFooter() {
+  return (
+    <ul className="flex gap-3 items-center text-white text-2xl">
+      <h2 className="text-xl font-semibold">SEGUICI</h2>
+      <li>
+        <a href="#">
+          <i className="fa-brands fa-facebook"></i>
+        </a>
+      </li>
+      <li>
+        <a href="#">
+          <i className="fa-brands fa-instagram"></i>
+        </a>
+      </li>
+      <li>
+        <a href="#">
+          <i className="fa-brands fa-youtube"></i>
+        </a>
+      </li>
+      <li>
+        <a href="#">
+          <i className="fa-brands fa-tiktok"></i>
+        </a>
+      </li>
+      <li>
+        <a href="#">
+          <i className="fa-brands fa-pinterest"></i>
+        </a>
+      </li>
+    </ul>
+  );
+}
