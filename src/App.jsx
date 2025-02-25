1eb8d772278027b28727e127ffe39efdc7b0da93
mattiadelmonaco <mattia.delmonaco@gmail.com>
2025-02-25 15:49:58 +0100

    feat: create defaultLyout and components Header and Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
-import { BrowserRouter, Routes, Route } from "react-router";
-import MoviesList from "./pages/MoviesList";
+import { BrowserRouter, Routes, Route, Router } from "react-router";
+import MoviesList from "./pages/Homepage";
 import SingleMovie from "./pages/SingleMovie";
+import DefaultLayout from "./layouts/DefaultLayout";
 
 export default function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route index path="/movies-list" element={<MoviesList />} />
-          <Route path="/single-movie" element={<SingleMovie />} />
+          <Route element={<DefaultLayout />}>
+            <Route index path="/homepage" element={<MoviesList />} />
+            <Route path="/single-movie" element={<SingleMovie />} />
+          </Route>
         </Routes>
       </BrowserRouter>
     </>
