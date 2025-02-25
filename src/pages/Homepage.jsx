18d2771797f49bf20173d564546003f04b699c2b
mattiadelmonaco <mattia.delmonaco@gmail.com>
2025-02-25 16:39:38 +0100

    feat: import use navigate and use in axios (catch) to send in NotFound if get error 404

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,11 +1,21 @@
 import { useEffect } from "react";
 import axios from "../api/axios";
+import { useNavigate } from "react-router";
 
 export default function Homepage() {
+  const navigate = useNavigate();
+
   const fetchMoviesList = () => {
-    axios.get("/movies").then((res) => {
-      console.log(res.data);
-    });
+    axios
+      .get("/movies")
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((err) => {
+        if (err.status === 404) {
+          navigate("/404");
+        }
+      });
   };
 
   useEffect(fetchMoviesList, []);
--- src/pages/SingleMovie.jsx
+++ src/pages/SingleMovie.jsx
@@ -1,14 +1,24 @@
 import axios from "../api/axios";
 import { useParams } from "react-router";
 import { useEffect } from "react";
+import { useNavigate } from "react-router";
 
 export default function SingleMovie() {
   const { id } = useParams();
 
+  const navigate = useNavigate();
+
   const fetchMovie = () => {
-    axios.get(`/movies/${id}`).then((res) => {
-      console.log(res.data);
-    });
+    axios
+      .get(`/movies/${id}`)
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((err) => {
+        if (err.status === 404) {
+          navigate("/404");
+        }
+      });
   };
 
   useEffect(fetchMovie, [id]);
