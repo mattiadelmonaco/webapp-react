745482a8d163ef11c9e382424e7a5d69c27c8ba8
mattiadelmonaco <mattia.delmonaco@gmail.com>
2025-02-25 17:09:47 +0100

    feat: change logo, add style and elements to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,21 @@
+import { Link } from "react-router";
+
 export default function Header() {
-  return <header>Header</header>;
+  return (
+    <header className="bg-black rounded-bl-4xl rounded-br-4xl">
+      <div className="container">
+        <div className="flex justify-between text-white items-center">
+          <Link to="/">
+            <img className="h-25" src="/logo-webapp.svg" alt="logo 138movies" />
+          </Link>
+          <h1 className="font-extrabold text-4xl">BENVENUTO SU 138movies</h1>
+          <ul>
+            <li>
+              <Link to="/"> HomePage</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </header>
+  );
 }
