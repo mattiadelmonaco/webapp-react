62321d9293f3d25ade61eb44efea1029411ea3b2
mattiadelmonaco <mattia.delmonaco@gmail.com>
2025-02-26 17:46:43 +0100

    feat: edit style

--- src/components/SingleMovieComp.jsx
+++ src/components/SingleMovieComp.jsx
@@ -1,18 +1,26 @@
 export default function SingleMovieComp({ movie }) {
   return (
     <div className="bg-white rounded-2xl mt-10">
-      <div className="flex justify-between p-10">
+      <div className="grid grid-cols-12 justify-between p-6">
         {movie.image ? (
-          <img src={movie.image} alt={movie.title} />
+          <div className="h-[500px] w-[350px] col-span-4">
+            <img
+              className="h-full w-full rounded-2xl"
+              src={movie.image}
+              alt={movie.title}
+            />
+          </div>
         ) : (
           <div>
             <i className="fa-solid fa-image text-9xl text-black"></i>
           </div>
         )}
-        <div className="space-y-5 text-xl">
-          <h2 className="text-center text-4xl font-extrabold mb-5">
-            {movie.title}
-          </h2>
+        <div className="space-y-5 text-xl col-span-8">
+          <h2 className="text-4xl font-extrabold mb-5">{movie.title}</h2>
+          <h3>
+            <strong>Media dei voti: </strong>
+            {movie.average_vote}
+          </h3>
           <h3>
             <strong>Regista: </strong>
             {movie.director}
