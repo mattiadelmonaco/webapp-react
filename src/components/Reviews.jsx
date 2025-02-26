61925d69f301389611bf4e70deaa2cfffe018f08
mattiadelmonaco <mattia.delmonaco@gmail.com>
2025-02-26 17:50:42 +0100

    fet: edit style

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -1,7 +1,7 @@
 export default function Reviews({ reviews }) {
   return (
     <div className="bg-white rounded-2xl my-10">
-      <h2 className="font-extrabold text-4xl text-center py-5">Recensioni</h2>
+      <h2 className="font-extrabold text-4xl text-center pt-5">Recensioni</h2>
       <ul className="p-5">
         {reviews.map((review) => {
           return (
